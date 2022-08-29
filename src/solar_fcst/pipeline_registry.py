9ea478abd0080de3b5c79018687270f88e868374
Sharabh Shukla <sharabh2019@gmail.com>
2022-08-28 22:52:33 -0700

    Added ds pipeline

--- src/solar_fcst/pipeline_registry.py
+++ src/solar_fcst/pipeline_registry.py
@@ -2,6 +2,8 @@
 from typing import Dict
 
 from kedro.pipeline import Pipeline, pipeline
+import solar_fcst.pipelines.preprocessing.pipeline as pps
+import solar_fcst.pipelines.data_science_fcst.pipeline as ds
 
 
 def register_pipelines() -> Dict[str, Pipeline]:
@@ -10,4 +12,7 @@ def register_pipelines() -> Dict[str, Pipeline]:
     Returns:
         A mapping from a pipeline name to a ``Pipeline`` object.
     """
-    return {"__default__": pipeline([])}
+    preprocess_raw_pv_pipeline = pps.create_pipeline()
+    data_science_pipeline = ds.create_pipeline()
+    return {"__default__": preprocess_raw_pv_pipeline + data_science_pipeline,
+            "preprocess_raw_pv": preprocess_raw_pv_pipeline}
